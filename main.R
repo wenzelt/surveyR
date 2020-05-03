8c3c5e5cd501e728d74b0495f0559562d865bb6a
Tom Wenzel <tomwenzel@hotmail.de>
2020-05-03 22:49:00 +0200

    t-test for R3

--- main.R
+++ main.R
@@ -76,10 +76,18 @@ rent_own <- select(singleSourceOfTruthAppended, A007, R101) #count ownership sit
 rent_own <- subset(rent_own, A007 == "Rent" | A007 == "Own")
 rent_own$A007 <- as.factor(rent_own$A007)
 t.test(rent_own$R101 ~ rent_own$A007, mu = 0 , alt = "two.sided", conf =0.95, var.eq = F, paired = F)
+qqnorm(rent_own$R101)
+qqline(rent_own$R101)
+
 ##alternative spelling##
 attach(rent_own)
 t.test(R101[A007=="Rent"], R101[A007=="Own"])
 detach()
 
 ###t-test for R3 according to doc###
-R3 <- select(singleSourceOfTruthAppended, A004, A005, A007, R101, R216, HP02_01,HP02_02,HP02_03,HP02_04,HP02_05, R501, R534, R528, R507)
+RQ3 <- select(singleSourceOfTruthAppended, A004, A005, A007, R101, R501, R534, R528, R507) #Rq3 Selection
+Children_OwnedDevices <- select(R3,A004,R101) #setting two vars for t test
+Children_OwnedDevices$A004 <- cut(Children_OwnedDevices$A004, breaks=c(0, 1, Inf))
+Children_OwnedDevices$A004 <- factor(Children_OwnedDevices$A004)
+t.test(Children_OwnedDevices$R101~Children_OwnedDevices$A004, mu = 0 , alt = "two.sided", conf =0.95, var.eq = F, paired = F)
+
