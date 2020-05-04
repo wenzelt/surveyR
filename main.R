3a0bcbc0aef40cff62605e2ba0d722282d003102
Tom Wenzel <tomwenzel@hotmail.de>
2020-05-04 12:49:33 +0200

    Added more t-tests

--- main.R
+++ main.R
@@ -71,6 +71,13 @@ dev.off()
 ### 3-Way ANOVA over categories ### 
 ### t-test for other categroeies###
 
+
+
+###t-test for R3 according to doc###
+RQ3 <- select(singleSourceOfTruthAppended, A004, A005, A007, R101, R501, R534, R528, R507) #Rq3 Selection
+RQ3$A004 <- cut(RQ3$A004, breaks=c(0, 1, Inf)) ## adding levels to children
+RQ3$A007 <- as.factor(RQ3$A007)
+
 ### t-test for owning and device amount ###
 rent_own <- select(singleSourceOfTruthAppended, A007, R101) #count ownership situation
 rent_own <- subset(rent_own, A007 == "Rent" | A007 == "Own")
@@ -84,10 +91,20 @@ attach(rent_own)
 t.test(R101[A007=="Rent"], R101[A007=="Own"])
 detach()
 
-###t-test for R3 according to doc###
-RQ3 <- select(singleSourceOfTruthAppended, A004, A005, A007, R101, R501, R534, R528, R507) #Rq3 Selection
 Children_OwnedDevices <- select(RQ3,A004,R101) #setting two vars for t test
-Children_OwnedDevices$A004 <- cut(Children_OwnedDevices$A004, breaks=c(0, 1, Inf))
 Children_OwnedDevices$A004 <- factor(Children_OwnedDevices$A004)
 t.test(Children_OwnedDevices$R101~Children_OwnedDevices$A004, mu = 0 , alt = "two.sided", conf =0.95, var.eq = F, paired = F)
 
+###t-test###DAily usage Children
+Children_DailyUsage <- select(RQ3,A004,R501) #setting two vars for t test
+Children_DailyUsage$R501 = as.factor(Children_DailyUsage$R501)
+t.test(Children_DailyUsage$R501~Children_DailyUsage$A004, mu = 0 , alt = "two.sided", conf =0.95, var.eq = F, paired = F)
+
+###t-test### Children and how many rooms have smart devices
+Children_DeviceLocation <- select(RQ3,A004,R528) #setting two vars for t test
+t.test(Children_DailyUsage$R528~Children_DailyUsage$A004, mu = 0 , alt = "two.sided", conf =0.95, var.eq = F, paired = F)
+
+###t-test### RentOwn_#rooms smart device is in
+RentOwn_DeviceLocation <- select(RQ3,A007,R528) #setting two vars for t test
+RentOwn_DeviceLocation <- subset(RentOwn_DeviceLocation, A007 == "Rent" | A007 == "Own")
+t.test(RentOwn_DeviceLocation$R528~RentOwn_DeviceLocation$A007, mu = 0 , alt = "two.sided", conf =0.95, var.eq = F, paired = F)
