1c362ab9d18ae2a7fc48c3298d8e2668113dfed1
TWenzel <twenzel@twenzels-air.speedport.ip>
2020-05-10 21:22:36 +0200

    more examples for non parametric tests

--- non_parametric_tests_rq.R
+++ non_parametric_tests_rq.R
@@ -12,6 +12,9 @@ RQ3 <- select(singleSourceOfTruthAppended, A004, A005, A007, R101, R501, R534, R
 attach(RQ3)
 ##testing wilcox test for children > 0 impact on amount of devices##
 RQ3$A004 <- cut(RQ3$A004, breaks=c(0, 1, Inf)) ## adding levels to children
-wilcox.test(R101~A004)
+wilcox.test(R101~A004) ##mann whitney u test
+kruskal.test(R101~A004) ## kruskal wallis test one way anova
+
 
 RQ3$A007 <- as.factor(RQ3$A007)
+wilcox.test(R101~A007 == "Rent"| A007 == "Own")
