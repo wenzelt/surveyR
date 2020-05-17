000d3e085684db9a8ddb4bf0bea8b5d7719234d9
TWenzel <twenzel@TWenzels-MacBook-Air.local>
2020-05-17 19:51:54 +0200

    minor fixes

--- bucketing.R
+++ bucketing.R
@@ -11,3 +11,4 @@ library(ggpubr)
 smartTvAnswers <- subset(singleSourceOfTruthAppended, R232_01 == "Smart TV" | R232_02 == "Smart TV"| R232_03 == "Smart TV")
 smartSpeakerAnswers <- subset(singleSourceOfTruthAppended, R232_01 == "Smart Speaker"| R232_01 == "Smart Lautsprecher"| R232_02 == "Smart Speaker"| R232_02 == "Smart Lautsprecher"| R232_03 == "Smart Speaker"| R232_03 == "Smart Lautsprecher")
 smartLightAnswers <- subset(singleSourceOfTruthAppended, R232_01 == "Smart Lightbulb" | R232_02 == "Smart Lightbulb"| R232_03 == "Smart Lightbulb" | R232_01 == "Smart GlÃ¼hbirne" | R232_02 == "Smart GlÃ¼hbirne"| R232_03 == "Smart GlÃ¼hbirne")
+
