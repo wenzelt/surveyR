a04b640af1acc55e411816676257d80f1fa46e0a
Tom Wenzel <tom.wenzel@tum.de>
2020-07-26 12:05:56 +0200

    Added more info to table and adjusted formatting
    Also inserted compiled latex pdf table preview in latex_tables

--- latex_table_generator.R
+++ latex_table_generator.R
@@ -5,4 +5,6 @@ library(xtable)
 # export LA_Mean x E201 Latex
 print(xtable(LA_E201_Latex, type = "latex",caption = "Summary statistic of correlation between Legislative Stance (LA) and Device Risk assessment (E201) measured on a 7-Point Likert scale"), file = "latex_tables/LA_MEANxE201.tex")
 
+# export LA_Mean x E201 Latex Interesting
+print(xtable(LA_E201_Latex_Interesting, type = "latex",caption = "Summary statistic of correlation between Legislative Stance (LA) and Device Risk assessment (E201) measured on a 7-Point Likert scale"), file = "latex_tables/LA_MEANxE201_Interesting.tex")
 
