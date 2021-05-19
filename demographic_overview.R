70e2123e2676d76b4cdfd91846aeca2c50b1acec
Tom Wenzel <tom.wenzel@limehome.de>
2021-05-19 08:20:09 -0400

    added gender overview data to demographic_overview

--- demographic_overview.R
+++ demographic_overview.R
@@ -1,5 +1,9 @@
 # Demographic overview of the sample. Please run 'install_dependencies.R first'
+install.packages('doBy')
+install.packages('janitor')
 
+library(doBy)
+library(janitor)
 
 demographic_data <-
   select(singleSourceOfTruthAppended,
@@ -8,13 +12,24 @@ demographic_data <-
          age)
 names(demographic_data)[names(demographic_data) == "Current Country of Residence"] <-
   "country"
+names(demographic_data)[names(demographic_data) == "Sex"] <-
+  "gender"
 
-#install.packages('doBy')
-library(doBy)
-#three people preferred not to enter their age
-summaryBy(
+attach(demographic_data)
+
+
+#3 people preferred not to enter their age
+age_data <- summaryBy(
   age ~ country,
   data = demographic_data,
   FUN = c(median, mean, sd, min, max),
   na.rm = TRUE
 )
+
+gender_data <- tabyl(demographic_data, country, gender) %>%
+  adorn_percentages("row") %>%
+  adorn_pct_formatting(digits = 1)
+
+asd <- bind_cols(age_data, gender_data[2:3])
+
+detach(demographic_data)
