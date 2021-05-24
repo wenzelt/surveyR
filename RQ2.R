212d8b819cc16315f2fdaed61a429745174fc6fd
Tom Wenzel <tom.wenzel@limehome.de>
2021-05-24 11:16:54 -0400

    added anotation of devices used

--- RQ2.R
+++ RQ2.R
@@ -921,8 +921,9 @@ summary(aov_content)
 # ANOVA II - E201 device risk ----
 
 calc_anova <- function(data, column_to_use) {
-  data <- data %>%
-    filter(column_to_use > 0)
+  
+  data <- data[data[[column_to_use]] >= 0, ]
+  
   
   means <-
     round(tapply(as.numeric(data[[column_to_use]]),
@@ -953,6 +954,34 @@ calc_anova <- function(data, column_to_use) {
   return(summary(aov_content))
 }
 calc_anova(country_anova, "E201_11")
+
+
+# Testing Device Risk-------------------------------------------------------------------------------
+# testing for country by perceived device risk
+
+# 1	E201_01	Device risk: Smart Coffee Maker
+# 2	E201_02	Device risk: Smart Dishwasher
+# 3	E201_03	Device risk: Smart Door Lock
+# 4	E201_04	Device risk: Smart Doorbell
+# 5	E201_05	Device risk: Smart Electricity Meter
+# 6	E201_06	Device risk: Smart Electrical Outlet
+# 7	E201_07	Device risk: Smart Fridge
+# 8	E201_08	Device risk: Smart Gardening Equipment
+# 9	E201_09	Device risk: Smart Heating/Cooling System
+# 10	E201_10	Device risk: Smart Home Monitoring System
+# 11	E201_11	Device risk: Smart Lightbulb
+# 12	E201_12	Device risk: Smart Oven
+# 13	E201_13	Device risk: Smart Robot
+# 14	E201_14	Device risk: Smart Speaker
+# 15	E201_15	Device risk: Smart Stove
+# 16	E201_16	Device risk: Smart TV
+# 17	E201_17	Device risk: Smart Thermostat
+# 18	E201_18	Device risk: Smart Toy
+# 19	E201_19	Device risk: Smart Vacuum Cleaner
+# 20	E201_20	Device risk: Smart Washing Machine
+
+
+
 
 
 detach(singleSourceOfTruthAppended)
