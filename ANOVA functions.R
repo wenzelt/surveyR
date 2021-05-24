5dc25ab54f4e25b39f6034bf11f15812a30c378c
Tom Wenzel <tom.wenzel@limehome.de>
2021-05-24 12:41:43 -0400

    added Perceived feature benefit to ANOVA analysis

--- ANOVA functions.R
+++ ANOVA functions.R
@@ -3,15 +3,19 @@ country_anova = select(
   `Current Country of Residence`,
   sebis_avg,
   R101,
-  E201_01:E201_20
+  A204_01:A204_06,
+  E201_01:E201_20,
+  A307_01:A307_10,
 )
 
+#Definition ANOVA function ----
 
-
+titles <- hash()
+titles$E201_01 
 
 calc_anova <- function(data, column_to_use) {
   if (!is.null(data[[column_to_use]])) {
-    data <- data[data[[column_to_use]] >= 0,]
+    data <- data[data[[column_to_use]] >= 0, ]
   } else{
     print("Column does not Exist")
   }
@@ -30,20 +34,23 @@ calc_anova <- function(data, column_to_use) {
     ccol = 'red',
     mean.labels = F,
     ylab = 'mean',
-    main = sprintf("Plot of %s means by region", column_to_use),
+    main = sprintf("Plot of '%s' means by region", titles[[column_to_use]]),
   )
   
   boxplot(
     data[[column_to_use]] ~ data$`Current Country of Residence`,
-    main = sprintf("Plot of %s means by region", column_to_use),
+    main = sprintf("Plot of '%s' means by region", titles[[column_to_use]]),
     xlab = "'region'",
-    ylab = column_to_use,
+    ylab = titles[[column_to_use]],
     col = rainbow(7)
   )
+  
+  
+  
   # F statistics = Variation among sample means / Variation within groups
   
   aov_content =  aov(data[[column_to_use]] ~ data$`Current Country of Residence`)
-  if ((summary(aov_content)[[1]][["Pr(>F)"]]) < 0.1) {
+  if ((summary(aov_content)[[1]][["Pr(>F)"]])[1] < 0.05) {
     tuk = TukeyHSD(aov_content)
     
     print(summary(aov_content))
@@ -54,30 +61,67 @@ calc_anova <- function(data, column_to_use) {
   
   
 }
+
+#E201 Device Risk ----
+titles$E201_01="Smart Coffee Maker"
+titles$E201_02="Smart Dishwasher"
+titles$E201_03="Smart Door Lock"
+titles$E201_04="Smart Doorbell"
+titles$E201_05="Smart Electricity Meter"
+titles$E201_06="Smart Electrical Outlet"
+titles$E201_07="Smart Fridge"
+titles$E201_08="Smart Gardening Equipment"
+titles$E201_09="Smart Heating/Cooling System"
+titles$E201_10="Smart Home Monitoring System"
+titles$E201_11="Smart Lightbulb"
+titles$E201_12="Smart Oven"
+titles$E201_13="Smart Robot"
+titles$E201_14="Smart Speaker"
+titles$E201_15="Smart Stove"
+titles$E201_16="Smart TV"
+titles$E201_17="Smart Thermostat"
+titles$E201_18="Smart Toy"
+titles$E201_19="Smart Vacuum Cleaner"
+titles$E201_20="Smart Washing Machine"
+
+
 #calc_anova(country_anova, "E201_11")
 #calc_anova(country_anova, "E201_14")
-calc_anova(country_anova, "E201_16") # significant Smart TV
-calc_anova(country_anova, "E201_19") # significant Smart Vacuum
-calc_anova(country_anova, "E201_18") # significant Smart Toy
+#calc_anova(country_anova, "E201_16") # significant Smart TV
+#calc_anova(country_anova, "E201_18") # significant Smart Toy
+#calc_anova(country_anova, "E201_19") # significant Smart Vacuum
 
 
-# 1	E201_01	Device risk: Smart Coffee Maker
-# 2	E201_02	Device risk: Smart Dishwasher
-# 3	E201_03	Device risk: Smart Door Lock
-# 4	E201_04	Device risk: Smart Doorbell
-# 5	E201_05	Device risk: Smart Electricity Meter
-# 6	E201_06	Device risk: Smart Electrical Outlet
-# 7	E201_07	Device risk: Smart Fridge
-# 8	E201_08	Device risk: Smart Gardening Equipment
-# 9	E201_09	Device risk: Smart Heating/Cooling System
-# 10	E201_10	Device risk: Smart Home Monitoring System
-# 11	E201_11	Device risk: Smart Lightbulb
-# 12	E201_12	Device risk: Smart Oven
-# 13	E201_13	Device risk: Smart Robot
-# 14	E201_14	Device risk: Smart Speaker
-# 15	E201_15	Device risk: Smart Stove
-# 16	E201_16	Device risk: Smart TV
-# 17	E201_17	Device risk: Smart Thermostat
-# 18	E201_18	Device risk: Smart Toy
-# 19	E201_19	Device risk: Smart Vacuum Cleaner
-# 20	E201_20	Device risk: Smart Washing Machine
+#A204 Perception of responsibility ----
+
+titles$A204_01="Keeping the Smart Home device software up-to-date"
+titles$A204_02="Ensuring my privacy"
+titles$A204_03="Protecting my Smart Home ecosystem as a whole"
+titles$A204_04="Keeping the Smart Home device secure"
+titles$A204_05="Fixing a hardware failure"
+titles$A204_06="Fixing a software failure"
+
+#calc_anova(country_anova, 'A204_04')
+#calc_anova(country_anova, 'A204_05') 
+
+#A307 Perceived Feature benefit ----
+
+titles$A307_01="Saving money"
+titles$A307_02="Saving energy"
+titles$A307_03="Increasing convenience"
+titles$A307_04="Enhancing leisure activities"
+titles$A307_05="Providing peace of mind"
+titles$A307_06="Providing comfort"
+titles$A307_07="Increasing safety"
+titles$A307_08="Providing care"
+titles$A307_09="Improving quality of life"
+titles$A307_10="Increasing property value"
+
+# calc_anova(country_anova,'A307_04')
+# calc_anova(country_anova,'A307_05')
+# calc_anova(country_anova,'A307_06')
+# calc_anova(country_anova,'A307_07')
+# calc_anova(country_anova,'A307_08')
+# calc_anova(country_anova,'A307_10')
+
+
