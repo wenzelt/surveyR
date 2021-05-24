dcfcb8cb4499a5715ff5be16c48bbb25405b19dd
Tom Wenzel <tom.wenzel@limehome.de>
2021-05-24 12:45:40 -0400

    Finished ANOVAs for Country Comparison

--- ANOVA functions.R
+++ ANOVA functions.R
@@ -8,14 +8,14 @@ country_anova = select(
   A307_01:A307_10,
 )
 
-#Definition ANOVA function ----
+#Definition ANOVA function -----
 
 titles <- hash()
-titles$E201_01 
+titles$E201_01
 
 calc_anova <- function(data, column_to_use) {
   if (!is.null(data[[column_to_use]])) {
-    data <- data[data[[column_to_use]] >= 0, ]
+    data <- data[data[[column_to_use]] >= 0,]
   } else{
     print("Column does not Exist")
   }
@@ -63,26 +63,26 @@ calc_anova <- function(data, column_to_use) {
 }
 
 #E201 Device Risk ----
-titles$E201_01="Smart Coffee Maker"
-titles$E201_02="Smart Dishwasher"
-titles$E201_03="Smart Door Lock"
-titles$E201_04="Smart Doorbell"
-titles$E201_05="Smart Electricity Meter"
-titles$E201_06="Smart Electrical Outlet"
-titles$E201_07="Smart Fridge"
-titles$E201_08="Smart Gardening Equipment"
-titles$E201_09="Smart Heating/Cooling System"
-titles$E201_10="Smart Home Monitoring System"
-titles$E201_11="Smart Lightbulb"
-titles$E201_12="Smart Oven"
-titles$E201_13="Smart Robot"
-titles$E201_14="Smart Speaker"
-titles$E201_15="Smart Stove"
-titles$E201_16="Smart TV"
-titles$E201_17="Smart Thermostat"
-titles$E201_18="Smart Toy"
-titles$E201_19="Smart Vacuum Cleaner"
-titles$E201_20="Smart Washing Machine"
+titles$E201_01 = "Smart Coffee Maker"
+titles$E201_02 = "Smart Dishwasher"
+titles$E201_03 = "Smart Door Lock"
+titles$E201_04 = "Smart Doorbell"
+titles$E201_05 = "Smart Electricity Meter"
+titles$E201_06 = "Smart Electrical Outlet"
+titles$E201_07 = "Smart Fridge"
+titles$E201_08 = "Smart Gardening Equipment"
+titles$E201_09 = "Smart Heating/Cooling System"
+titles$E201_10 = "Smart Home Monitoring System"
+titles$E201_11 = "Smart Lightbulb"
+titles$E201_12 = "Smart Oven"
+titles$E201_13 = "Smart Robot"
+titles$E201_14 = "Smart Speaker"
+titles$E201_15 = "Smart Stove"
+titles$E201_16 = "Smart TV"
+titles$E201_17 = "Smart Thermostat"
+titles$E201_18 = "Smart Toy"
+titles$E201_19 = "Smart Vacuum Cleaner"
+titles$E201_20 = "Smart Washing Machine"
 
 
 #calc_anova(country_anova, "E201_11")
@@ -94,28 +94,28 @@ titles$E201_20="Smart Washing Machine"
 
 #A204 Perception of responsibility ----
 
-titles$A204_01="Keeping the Smart Home device software up-to-date"
-titles$A204_02="Ensuring my privacy"
-titles$A204_03="Protecting my Smart Home ecosystem as a whole"
-titles$A204_04="Keeping the Smart Home device secure"
-titles$A204_05="Fixing a hardware failure"
-titles$A204_06="Fixing a software failure"
+titles$A204_01 = "Keeping the Smart Home device software up-to-date"
+titles$A204_02 = "Ensuring my privacy"
+titles$A204_03 = "Protecting my Smart Home ecosystem as a whole"
+titles$A204_04 = "Keeping the Smart Home device secure"
+titles$A204_05 = "Fixing a hardware failure"
+titles$A204_06 = "Fixing a software failure"
 
 #calc_anova(country_anova, 'A204_04')
-#calc_anova(country_anova, 'A204_05') 
+#calc_anova(country_anova, 'A204_05')
 
 #A307 Perceived Feature benefit ----
 
-titles$A307_01="Saving money"
-titles$A307_02="Saving energy"
-titles$A307_03="Increasing convenience"
-titles$A307_04="Enhancing leisure activities"
-titles$A307_05="Providing peace of mind"
-titles$A307_06="Providing comfort"
-titles$A307_07="Increasing safety"
-titles$A307_08="Providing care"
-titles$A307_09="Improving quality of life"
-titles$A307_10="Increasing property value"
+titles$A307_01 = "Saving money"
+titles$A307_02 = "Saving energy"
+titles$A307_03 = "Increasing convenience"
+titles$A307_04 = "Enhancing leisure activities"
+titles$A307_05 = "Providing peace of mind"
+titles$A307_06 = "Providing comfort"
+titles$A307_07 = "Increasing safety"
+titles$A307_08 = "Providing care"
+titles$A307_09 = "Improving quality of life"
+titles$A307_10 = "Increasing property value"
 
 # calc_anova(country_anova,'A307_04')
 # calc_anova(country_anova,'A307_05')
@@ -123,5 +123,3 @@ titles$A307_10="Increasing property value"
 # calc_anova(country_anova,'A307_07')
 # calc_anova(country_anova,'A307_08')
 # calc_anova(country_anova,'A307_10')
-
-
