fa25d6f12a270bd0bae81d2422344d900c55002e
Tom Wenzel <tom.wenzel@limehome.de>
2021-05-30 15:04:20 +0200

    added linear modelling to device risk

--- RQ1_Linear_Regression.R
+++ RQ1_Linear_Regression.R
@@ -13,7 +13,7 @@ la_regression = select(
 
 #Definition LR function -----
 calc_lr <- function(data) {
-  devices.lm <- lm(formula = data$LA_Mean ~ data$R101 + age + Sex,
+  devices.lm <- lm(formula = data$LA_Mean ~ data$A204_04 + data$age + data$Sex,
                    data = data)
   devices$value <- devices.lm$fitted.values
   head(devices)
@@ -32,5 +32,51 @@ calc_lr <- function(data) {
   abline(b = 1, a = 0)
   summary(devices.lm)
 }
-calc_lr(la_regression)
 
+data = la_regression
+lm_responsibility <- lm(formula = data$LA_Mean ~ data$E201_16,
+                 data = data)
+if (!is.null(data[[column_to_use]])) {
+  data <- data[data[[column_to_use]] >= 0,]
+} else{
+  print("Column does not Exist")
+}
+plot(
+  x = data$LA_Mean,
+  # True values on x-axis
+  y = data$E201_16,
+  # fitted values on y-axis
+  xlab = "Country perception",
+  ylab = "Risk",
+  main = "Regression fits of Risk values"
+)
+summary(lm_responsibility)
+
+plot(
+  x = devices.lm$model$R101,
+  # True values on x-axis
+  y = devices.lm$fitted.values,
+  # fitted values on y-axis
+  xlab = "True Values",
+  ylab = "Model Fitted Values",
+  main = "Regression fits of diamond values"
+)
+
+
+
+devices$value <- devices.lm$fitted.values
+head(devices)
+
+
+plot(
+  x = devices.lm$model$R101,
+  # True values on x-axis
+  y = devices.lm$fitted.values,
+  # fitted values on y-axis
+  xlab = "True Values",
+  ylab = "Model Fitted Values",
+  main = "Regression fits of diamond values"
+)
+
+abline(b = 1, a = 0)
+summary(devices.lm)
