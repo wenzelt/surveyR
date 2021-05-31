3f45a6d177966049ec401e3037da3a3f797fa5ed
Tom Wenzel <tom.wenzel@limehome.de>
2021-05-31 13:53:25 +0200

    linear regression household

---
+++ RQ3_ANOVA.R
@@ -0,0 +1,44 @@
+house_anova = select(singleSourceOfTruthAppended, A007, R101)
+
+calc_anova_house <- function(d) {
+  # pdf(sprintf("Plot_LA_MEAN_%s.pdf", titles[[names(d[2])]]))
+  
+  print(sprintf("Running ANOVA on %s and '%s'", names(d[1]), titles[[names(d[2])]]))
+  d[, 2][d[, 2] < 0] = NA #remove negative values
+  
+  
+  means <-
+    round(tapply(as.numeric(d[[2]]),
+                 d$A007,
+                 mean),
+          digits = 2)
+  
+  plotmeans(
+    d[[2]] ~ d$A007,
+    digits = 2,
+    ccol = 'red',
+    mean.labels = F,
+    ylab = 'mean',
+  )
+  
+  boxplot(
+    d[[2]] ~ d$A007,
+    xlab = "'region'",
+    ylab = names(d[[2]]),
+    col = rainbow(7)
+  )
+  
+  aov_content =  aov(d[[2]] ~ d$A007)
+  if ((summary(aov_content)[[1]][["Pr(>F)"]])[1] < 0.1) {
+    tuk = TukeyHSD(aov_content)
+    
+    print(summary(aov_content))
+    print(tuk)
+    plot(tuk)
+    print(means)
+    
+    # dev.off()
+  }
+}
+
+calc_anova_house(house_anova)
