d37bf9fd5ea589ed1e915c461582c545190b8e78
Tom Wenzel <tom.wenzel@limehome.de>
2021-05-31 12:08:16 +0200

    updated titles

--- RQ1_Linear_Regression.R
+++ RQ1_Linear_Regression.R
@@ -1,5 +1,6 @@
 
 
+
 #E201 Device Risk ----
 set_device_titles <- function() {
   titles <- hash()
@@ -24,6 +25,29 @@ set_device_titles <- function() {
   titles$E201_18 = "Smart Toy"
   titles$E201_19 = "Smart Vacuum Cleaner"
   titles$E201_20 = "Smart Washing Machine"
+  
+  #A204 Perception of responsibility ----
+  
+  titles$A204_01 = "Keeping the Smart Home device software up-to-date"
+  titles$A204_02 = "Ensuring my privacy"
+  titles$A204_03 = "Protecting my Smart Home ecosystem as a whole"
+  titles$A204_04 = "Keeping the Smart Home device secure"
+  titles$A204_05 = "Fixing a hardware failure"
+  titles$A204_06 = "Fixing a software failure"
+  
+  
+  #A307 Perceived Feature benefit ----
+  
+  titles$A307_01 = "Saving money"
+  titles$A307_02 = "Saving energy"
+  titles$A307_03 = "Increasing convenience"
+  titles$A307_04 = "Enhancing leisure activities"
+  titles$A307_05 = "Providing peace of mind"
+  titles$A307_06 = "Providing comfort"
+  titles$A307_07 = "Increasing safety"
+  titles$A307_08 = "Providing care"
+  titles$A307_09 = "Improving quality of life"
+  titles$A307_10 = "Increasing property value"
 }
 
 set_device_titles()
@@ -38,32 +62,42 @@ data = select(
   A204_01:A204_06,
   E201_01:E201_20,
   A307_01:A307_10,
-  `Current Country of Residence`
+  `Current Country of Residence`,
+  muipc_PerceivedSur_avg
 )
 
 
 #Definition LR function -----
 calc_lr <- function(d) {
+  # pdf(sprintf("Plot_LA_MEAN_%s_by_device_Risk.pdf", titles[[names(d[2])]]))
+  
+  print(sprintf("Running Regression on %s and '%s'", names(d[1]), titles[[names(d[2])]]))
   d = d[1:2]
   d[, 2][d[, 2] < 0] = NA #remove negative values
-  lmtemp = lm(d[[1]] ~ d[[2]] + data$age , data = d)
+  lmtemp = lm(d[[1]] ~ d[[2]] + data$Sex + data$age , data = d)
   plot(
     lmtemp$residuals,
     pch = 16,
     col = "red",
     main = sprintf("'%s'~ '%s' LR residuals", names(d[1]), titles[[names(d[2])]]),
   )
-  plot(d,
-       col = 'blue',
-       main = sprintf("'%s'~ '%s' LR values", names(d[1]), titles[[names(d[2])]]),
+  plot(
+    d,
+    ylab = sprintf("'%s' risk assessment", titles[[names(d[2])]]),
+    xlab = sprintf("'%s' regulatory perception", names(d[1])),
+    col = 'blue',
+    main = sprintf("'%s'~ '%s' LR values", names(d[1]), titles[[names(d[2])]]),
   )
   abline(lmtemp)
   print(summary(lmtemp))
-  
+  # dev.off()
 }
-
 
 
 calc_lr(select(data, LA_Mean, E201_11))
 calc_lr(select(data, LA_Mean, E201_14))
 calc_lr(select(data, LA_Mean, E201_16))
+
+calc_lr(select(data, LA_Mean, muipc_PerceivedSur_avg))
+
+
