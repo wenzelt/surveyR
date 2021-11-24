b8e791bb41cb08840ec420a656de33fb42dce422
Twenzel <tomwenzel@hotmail.de>
2021-11-24 11:04:34 +0100

    investigation difference prolific and age.

---
+++ RQ0_investigation_age_gap.R
@@ -0,0 +1,10 @@
+# New investigation to age differences in participants 
+library(stringr)
+
+attach(singleSourceOfTruthAppended)
+
+
+split_age = select(singleSourceOfTruthAppended, A001)
+split_age = str_split_fixed(split_age$A001, "-", 2)
+ages = cbind(select(singleSourceOfTruthAppended, age), split_age)
+ages$flag = apply(ages, 1, function(x){ ifelse(x["age"] >= as.numeric(x["1"]) +1  &&x["age"] <= as.numeric(x["2"]) + 1 , 1, 2) })
